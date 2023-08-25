b4c5091101b77d4fd326f2ef951bf1cc48a43984
michael <michaelhuggins@protonmail.com>
2023-08-25 12:29:22 -0700

    added src/make_lidar_intensity.py

---
+++ src/make_lidar_intensity.py
@@ -0,0 +1,70 @@
+#!/bin/bash/python
+#
+# requires pdal-python
+# conda install -c conda-forge python-pdal
+# Usage:
+# python make_lidar_intensity.py \
+#   --tile=10TEL0509245547 \
+#   --out_dir=/path/to/out_dir
+
+from pathlib import Path
+import argparse
+import pdal
+
+
+def parse_arguments():
+    '''parses the arguments, returns args'''
+
+    # init parser
+    parser = argparse.ArgumentParser()
+
+    # add args
+    parser.add_argument(
+        '--tile',
+        type=str,
+        required=True,
+        help='Path to Lidar tile.'
+    )
+
+    parser.add_argument(
+        '--out_dir',
+        type=str,
+        required=True,
+        help='Path to output directory.'
+    )
+    
+    # parse the args
+    args = parser.parse_args()
+
+    return(args)
+
+
+def intense_pipe(tile, out_dir):
+    '''
+    writes 1m tif from mean intensity of first returns.
+    '''
+    
+    # define stages
+    reader = pdal.Reader.las(tile)
+    filter = pdal.Filter.returns(group='first')
+    writer = pdal.Writer.gdal(
+        filename= str(Path(out_dir) / Path(tile).stem + '.tif'),
+        dimension='Intensity',
+        data_type='uint16_t',
+        output_type='mean',
+        resolution='1'
+        )
+    
+    # create pipeline from stages
+    pipeline = pdal.Pipeline()
+    pipeline |= filter
+    pipeline |= writer
+
+    # execute pipeline
+    pipeline.execute()
+    
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    intense_pipe(args.tile, args.out_dir)
+    
