257d8d6e825c2ca1de5ece37e7d456819816f72a
michael <michaelhuggins@protonmail.com>
2023-08-31 10:05:52 -0700

    changed conflicting variable name cols to feat_cols in src/make_features.py.  Also temporarily changed it to skip first iteration, after use remember to delete code chunk at line 261

--- src/make_features.py
+++ src/make_features.py
@@ -13,14 +13,16 @@
 
 from pathlib import Path
 import math
-#from joblib import Parallel, delayed
+import joblib
+from joblib import Parallel, delayed, dump, load
+import contextlib
+import os
 
 import geopandas as gpd
 import numpy as np
 import pandas as pd
 import rioxarray
 from tqdm import tqdm
-from xrspatial.multispectral import ndvi, savi
 import argparse
 
 
@@ -67,6 +69,15 @@ def parse_arguments():
     )
     
     parser.add_argument(
+        '--n_jobs',
+        type=int,
+        required=False,
+        default=-1,
+        help='''Maximum number of concurrently running jobs.
+        Defaults to -1, which means all CPUs are used.'''
+    )
+    
+    parser.add_argument(
         '--label',
         type=str,
         required=False,
@@ -91,8 +102,138 @@ def parse_arguments():
 
     return(args)
 
+@contextlib.contextmanager
+def tqdm_joblib(tqdm_object):
+    '''
+    Context manager to patch joblib to report into
+    tqdm progress bar given as argument
+    '''
+    class TqdmBatchCompletionCallback(joblib.parallel.BatchCompletionCallBack):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
 
-def make_model_inputs(crown_path, cols, tif_path, save_path, y, IDcolumn, label=None,):
+        def __call__(self, *args, **kwargs):
+            tqdm_object.update(n=self.batch_size)
+            return super().__call__(*args, **kwargs)
+
+    old_batch_callback = joblib.parallel.BatchCompletionCallBack
+    joblib.parallel.BatchCompletionCallBack = TqdmBatchCompletionCallback
+    try:
+        yield tqdm_object
+    finally:
+        joblib.parallel.BatchCompletionCallBack = old_batch_callback
+        tqdm_object.close() 
+        
+
+def inner_func(row, xa, masked_rgi, masked_ndvi, r_,
+               g_, b_, n_, IDcolumn, label, bins):
+    '''guts of make_model_input to run in parallel'''
+    
+    # this is f!#@ed up, but Parallel and the
+    # context manager make it necessary for some reason
+    import rioxarray
+    
+    # calculate luminosity fractions
+    lum = xa.luminosity.rio.clip([row.geometry]).to_numpy().flatten()
+    lum_tot = lum.shape[0]
+    lum_fracs = [((lum < f).sum() - (lum < f - 0.1).sum()) / lum_tot for f in bins]
+
+    # calculate rgi fracs
+    rgi = masked_rgi.rio.clip([row.geometry]).to_numpy().flatten()
+    rgi = rgi[~np.isnan(rgi)]
+    rgi_tot = len(rgi)
+    if rgi_tot == 0:
+        rgi_fracs = [-99] * 10
+    else:
+        rgi_fracs = [((rgi < f).sum() - (rgi < f - 0.1).sum()) / rgi_tot for f in bins]
+        
+    # and normed pix colr fracs
+    r = r_.rio.clip([row.geometry]).to_numpy().flatten()
+    r = r[~np.isnan(r)]
+    c_tot = len(r)
+    
+    g = g_.rio.clip([row.geometry]).to_numpy().flatten()
+    g = g[~np.isnan(g)]
+
+    b = b_.rio.clip([row.geometry]).to_numpy().flatten()
+    b = b[~np.isnan(b)]
+
+    n = n_.rio.clip([row.geometry]).to_numpy().flatten()
+    n = n[~np.isnan(n)]
+
+    if c_tot == 0:
+        r_fracs = [-99] * 10
+        g_fracs = [-99] * 10
+        b_fracs = [-99] * 10
+        n_fracs = [-99] * 10
+    else:
+        r_fracs = [((r < f).sum() - (r < f - 0.1).sum()) / c_tot for f in bins]
+        g_fracs = [((g < f).sum() - (g < f - 0.1).sum()) / c_tot for f in bins]
+        b_fracs = [((b < f).sum() - (b < f - 0.1).sum()) / c_tot for f in bins]
+        n_fracs = [((n < f).sum() - (n < f - 0.1).sum()) / c_tot for f in bins]
+                
+    # calculate means and stdevs
+    if rgi_tot == 0:
+        ndvi_mean, ndvi_std = -99, -99
+        rgi_mean, rgi_std = -99, -99
+        savi_mean, savi_std = -99, -99
+        r_mean, r_std = -99, -99
+        g_mean, g_std = -99, -99
+        b_mean, b_std = -99, -99
+        n_mean, n_std = -99, -99
+    else:
+        #NOTE: .values * 1 casts 1 item DataArray to float
+        ndvi_mean = masked_ndvi.mean(skipna=True).values * 1
+        ndvi_std = masked_ndvi.std(skipna=True).values * 1
+
+        rgi_mean = rgi.mean()
+        rgi_std = rgi.std()
+
+        savi_mean = xa.SAVI.mean(skipna=True).values * 1
+        savi_std = xa.SAVI.std(skipna=True).values * 1
+
+        # ensure no infs
+        if math. isinf(ndvi_mean):
+            raise Exception('ndvi_mean is inf')
+        if math. isinf(ndvi_std):
+            raise Exception('ndvi_std is inf')
+        if math. isinf(savi_mean):
+            raise Exception('ndvi_mean is inf')
+        if math. isinf(savi_std):
+            raise Exception('ndvi_std is inf')
+
+
+        r_mean = r.mean()
+        r_std = r.std()
+
+        g_mean = g.mean()
+        g_std = g.std()
+
+        b_mean = b.mean()
+        b_std = b.std()
+
+        n_mean = n.mean()
+        n_std = n.std()
+
+    if label is None:
+        row[label] = -99
+
+    return (
+        [row[IDcolumn], (row[label] + 1) / 2] +
+        lum_fracs +
+        rgi_fracs + 
+        r_fracs + 
+        g_fracs + 
+        b_fracs + 
+        n_fracs +
+        [ndvi_mean, ndvi_std, rgi_mean, rgi_std, savi_mean, savi_std] +
+        [r_mean, r_std, g_mean, g_std, b_mean, b_std, n_mean, n_std]
+        )
+    
+    
+
+
+def make_model_inputs(crown_path, cols, tif_path, save_path, y, IDcolumn, n_jobs, label=None,):
     '''
     Returns DataFrame with features for use in classification model.
     The resulting DataFrame has 'ID' column which matches that in crowns.
@@ -117,6 +258,10 @@ def make_model_inputs(crown_path, cols, tif_path, save_path, y, IDcolumn, label=
         if j == 0:
             previous = j
             continue
+        # TODO: REMOVE THIS TEMPORARY CHANGE
+        if j == 1:
+            previous = j
+            continue
         
         print(f'\t\t\t-- on {i} of {len(chunks)} --')
         # get the extent of the crowns
@@ -129,7 +274,6 @@ def make_model_inputs(crown_path, cols, tif_path, save_path, y, IDcolumn, label=
             maxx=xmax,
             maxy=ymax
         ).to_dataset(name='band_data')
-
 
         # normalized the band_data
         print(f'\t\t--normalizing (step 1/7)...')
@@ -185,116 +329,66 @@ def make_model_inputs(crown_path, cols, tif_path, save_path, y, IDcolumn, label=
         # read crowns
         crowns = gpd.read_parquet(crown_path).iloc[previous:j, :][cols]
         
-        data = []
-        masked_count = 0
-        total = len(crowns)
+        # make disk storage
+        mem_dir = Path.cwd() / 'joblib_memmap'
+            
+        try:
+            os.mkdir(mem_dir)
+        except FileExistsError:
+            pass
+        
+      
+        memmap_path0 = mem_dir / 'data_memmap0'
+        dump(xa, memmap_path0)
+        xa = load(memmap_path0, mmap_mode='r')
+        
+        memmap_path1 = mem_dir / 'data_memmap1'
+        dump(masked_rgi, memmap_path1)
+        masked_rgi = load(memmap_path1, mmap_mode='r')
+        
+        memmap_path2 = mem_dir / 'data_memmap2'
+        dump(masked_ndvi, memmap_path2)
+        masked_ndvi = load(memmap_path2, mmap_mode='r')
+        
+        memmap_path3 = mem_dir / 'data_memmap3'
+        dump(r_, memmap_path3)
+        r_ = load(memmap_path3, mmap_mode='r')
+        
+        memmap_path4 = mem_dir / 'data_memmap4'
+        dump(g_, memmap_path4)
+        g_ = load(memmap_path4, mmap_mode='r')
+        
+        memmap_path5 = mem_dir / 'data_memmap5'
+        dump(b_, memmap_path5)
+        b_ = load(memmap_path5, mmap_mode='r')
+        
+        memmap_path6 = mem_dir / 'data_memmap6'
+        dump(n_, memmap_path6)
+        n_ = load(memmap_path6, mmap_mode='r')
+        
+        # attach values to crowns
         bins = np.arange(0.1, 1.1, 0.1)
-        with tqdm(total=total) as progress_bar:
-            for _, row in crowns.iterrows():
-                # calculate luminosity fractions
-                lum = xa.luminosity.rio.clip([row.geometry]).to_numpy().flatten()
-                lum_tot = lum.shape[0]
-                lum_fracs = [((lum < f).sum() - (lum < f - 0.1).sum()) / lum_tot for f in bins]
+        with tqdm_joblib(tqdm(desc="My calculation", total=len(crowns))) as progress_bar:
+            data = Parallel(n_jobs=n_jobs)(
+                delayed(inner_func)
+                (
+                    row,
+                    xa,
+                    masked_rgi,
+                    masked_ndvi,
+                    r_,
+                    g_,
+                    b_,
+                    n_,
+                    IDcolumn,
+                    label,
+                    bins
+                )
+                for _, row in crowns.iterrows()
+            )
+        
 
-                # calculate rgi fracs
-                rgi = masked_rgi.rio.clip([row.geometry]).to_numpy().flatten()
-                rgi = rgi[~np.isnan(rgi)]
-                rgi_tot = len(rgi)
-                if rgi_tot == 0:
-                    rgi_fracs = [-99] * 10
-                else:
-                    rgi_fracs = [((rgi < f).sum() - (rgi < f - 0.1).sum()) / rgi_tot for f in bins]
-                    
-                # and normed pix colr fracs
-                r = r_.rio.clip([row.geometry]).to_numpy().flatten()
-                r = r[~np.isnan(r)]
-                c_tot = len(r)
-                
-                g = g_.rio.clip([row.geometry]).to_numpy().flatten()
-                g = g[~np.isnan(g)]
-
-                b = b_.rio.clip([row.geometry]).to_numpy().flatten()
-                b = b[~np.isnan(b)]
-
-                n = n_.rio.clip([row.geometry]).to_numpy().flatten()
-                n = n[~np.isnan(n)]
-
-                if c_tot == 0:
-                    r_fracs = [-99] * 10
-                    g_fracs = [-99] * 10
-                    b_fracs = [-99] * 10
-                    n_fracs = [-99] * 10
-                else:
-                    r_fracs = [((r < f).sum() - (r < f - 0.1).sum()) / c_tot for f in bins]
-                    g_fracs = [((g < f).sum() - (g < f - 0.1).sum()) / c_tot for f in bins]
-                    b_fracs = [((b < f).sum() - (b < f - 0.1).sum()) / c_tot for f in bins]
-                    n_fracs = [((n < f).sum() - (n < f - 0.1).sum()) / c_tot for f in bins]
-                            
-                # calculate means and stdevs
-                if rgi_tot == 0:
-                    ndvi_mean, ndvi_std = -99, -99
-                    rgi_mean, rgi_std = -99, -99
-                    savi_mean, savi_std = -99, -99
-                    r_mean, r_std = -99, -99
-                    g_mean, g_std = -99, -99
-                    b_mean, b_std = -99, -99
-                    n_mean, n_std = -99, -99
-                else:
-                    #NOTE: .values * 1 casts 1 item DataArray to float
-                    ndvi_mean = masked_ndvi.mean(skipna=True).values * 1
-                    ndvi_std = masked_ndvi.std(skipna=True).values * 1
-
-                    rgi_mean = rgi.mean()
-                    rgi_std = rgi.std()
-
-                    savi_mean = xa.SAVI.mean(skipna=True).values * 1
-                    savi_std = xa.SAVI.std(skipna=True).values * 1
-
-                    # ensure no infs
-                    if math. isinf(ndvi_mean):
-                        raise Exception('ndvi_mean is inf')
-                    if math. isinf(ndvi_std):
-                        raise Exception('ndvi_std is inf')
-                    if math. isinf(savi_mean):
-                        raise Exception('ndvi_mean is inf')
-                    if math. isinf(savi_std):
-                        raise Exception('ndvi_std is inf')
-
-
-                    r_mean = r.mean()
-                    r_std = r.std()
-
-                    g_mean = g.mean()
-                    g_std = g.std()
-
-                    b_mean = b.mean()
-                    b_std = b.std()
-
-                    n_mean = n.mean()
-                    n_std = n.std()
-
-                if label is None:
-                    row[label] = -99
-
-                data.append(
-                    [row[IDcolumn], (row[label] + 1) / 2] +
-                    lum_fracs +
-                    rgi_fracs + 
-                    r_fracs + 
-                    g_fracs + 
-                    b_fracs + 
-                    n_fracs +
-                    [ndvi_mean, ndvi_std, rgi_mean, rgi_std, savi_mean, savi_std] +
-                    [r_mean, r_std, g_mean, g_std, b_mean, b_std, n_mean, n_std]
-                    )
-
-                #count polygon if has masked pixels            
-                if rgi_tot < len(xa.rgi.rio.clip([row.geometry]).to_numpy().flatten()):
-                    masked_count = masked_count + 1
-
-                progress_bar.update(1)
-
-        cols = [IDcolumn, 'label',
+        feat_cols = [IDcolumn, 'label',
                 'lum10', 'lum20', 'lum30', 'lum40', 'lum50', 'lum60' ,'lum70', 'lum80', 'lum90', 'lum100',
                 'rgi10', 'rgi20', 'rgi30', 'rgi40', 'rgi50', 'rgi60' ,'rgi70', 'rgi80', 'rgi90', 'rgi100',
                 'r10', 'r20', 'r30', 'r40', 'r50', 'r60' ,'r70', 'r80', 'r90', 'r100',
@@ -304,7 +398,7 @@ def make_model_inputs(crown_path, cols, tif_path, save_path, y, IDcolumn, label=
                 'ndvi_mean', 'ndvi_std', 'rgi_mean', 'rgi_std', 'savi_mean', 'savi_std',
                 'r_mean', 'r_std', 'g_mean', 'g_std', 'b_mean', 'b_std', 'n_mean', 'n_std']
 
-        data = pd.DataFrame(data, columns=cols)
+        data = pd.DataFrame(data, columns=feat_cols)
         dst = save_path / f'features_{y}_{i}.parquet'
         data.to_parquet(dst)
         print(y, 'saved to ', str(dst))
@@ -326,6 +420,7 @@ if __name__ == '__main__':
         args.save_path,
         args.year,
         args.IDcolumn,
+        args.n_jobs,
         label=args.label
         )
 # %%
